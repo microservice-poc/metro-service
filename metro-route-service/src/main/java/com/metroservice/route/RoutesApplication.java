6389af63839af6acfe474122649029b0ffe9950d
Shibu TN <tnshibu@yahoo.com>
2018-08-07 17:29:40 +0530

    enabled Eureka in schedule service

--- metro-route-service/src/main/java/com/metroservice/route/RoutesApplication.java
+++ metro-route-service/src/main/java/com/metroservice/route/RoutesApplication.java
@@ -2,7 +2,6 @@ package com.metroservice.route;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
