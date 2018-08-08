f2ead673add3ee40e71e89512c416b8ab96da85a
Shibu TN <tnshibu@yahoo.com>
2018-08-08 20:49:37 +0530

    bulk revert from pre version

--- run-all-metro-services/40_metro-route-server.bat
+++ run-all-metro-services/40_metro-route-server.bat
@@ -3,3 +3,8 @@ SET JAVA_HOME=D:\Java\JDK_v1.8.0_101\
 
 
 "%JAVA_HOME%/bin/java" -jar %DEVSPACE_DIR%\com\metroservice\metro-route-service\0.0.1-SNAPSHOT\metro-route-service-0.0.1-SNAPSHOT-spring-boot.jar
+
+@echo off
+rem "%JAVA_HOME%/bin/java" -Dserver.port=8101 -jar %DEVSPACE_DIR%\com\metroservice\metro-route-service\0.0.1-SNAPSHOT\metro-route-service-0.0.1-SNAPSHOT-spring-boot.jar
+
+rem "%JAVA_HOME%/bin/java" -Dserver.port=8102 -jar %DEVSPACE_DIR%\com\metroservice\metro-route-service\0.0.1-SNAPSHOT\metro-route-service-0.0.1-SNAPSHOT-spring-boot.jar
