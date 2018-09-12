9dc4deb39672f1a63db290d112d621d0e5760d45
Shibu TN <tnshibu@yahoo.com>
2018-09-12 18:34:08 +0530

    metro-ui-service

---
+++ run-all-metro-services/80_metro-ui-server.bat
@@ -0,0 +1,5 @@
+set DEVSPACE_DIR=D:\devspace\repository
+SET JAVA_HOME=D:\Java\JDK_v1.8.0_101\
+
+
+"%JAVA_HOME%\bin\java" -jar %DEVSPACE_DIR%\com\metroservice\metro-ui-service\0.0.1-SNAPSHOT\metro-ui-service-0.0.1-SNAPSHOT-spring-boot.jar
