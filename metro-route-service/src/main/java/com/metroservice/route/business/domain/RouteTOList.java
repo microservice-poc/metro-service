4df3426c010693efdae394dccb103722a1e25831
Shibu TN <tnshibu@yahoo.com>
2018-09-14 04:16:12 +0530

    station

--- metro-route-service/src/main/java/com/metroservice/route/business/domain/RouteTOList.java
+++ metro-route-service/src/main/java/com/metroservice/route/business/domain/RouteTOList.java
@@ -1,14 +1,20 @@
 package com.metroservice.route.business.domain;
 
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Data;
 
+//@Getter @Setter 
+@Data
 public class RouteTOList {
 	private List<RouteTO> routeList ;
 
-    public List<RouteTO> getRouteList() { 
-		return routeList;
-	}
-	public void setRouteList(List<RouteTO> routeList) { 
-		this.routeList = routeList;
-	}
+//    public List<RouteTO> getRouteList() { 
+//		return routeList;
+//	}
+//	public void setRouteList(List<RouteTO> routeList) { 
+//		this.routeList = routeList;
+//	}
 }
