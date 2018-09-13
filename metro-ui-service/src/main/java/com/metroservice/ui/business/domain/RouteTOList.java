1acfc1c3bfeafc6fc93c32aeed833e37a5294c1e
Shibu TN <tnshibu@yahoo.com>
2018-09-14 03:31:22 +0530

    Routename impl and lombok

--- metro-ui-service/src/main/java/com/metroservice/ui/business/domain/RouteTOList.java
+++ metro-ui-service/src/main/java/com/metroservice/ui/business/domain/RouteTOList.java
@@ -2,13 +2,20 @@ package com.metroservice.ui.business.domain;
 
 import java.util.List;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Data;
+
+//@Getter @Setter 
+@Data
 public class RouteTOList {
 	private List<RouteTO> routeList ;
 
-    public List<RouteTO> getRouteList() { 
-		return routeList;
-	}
-	public void setRouteList(List<RouteTO> routeList) { 
-		this.routeList = routeList;
-	}
+//    public List<RouteTO> getRouteList() { 
+//		return routeList;
+//	}
+//	public void setRouteList(List<RouteTO> routeList) { 
+//		this.routeList = routeList;
+//	}
 }
