4ac8af10839345a817d85fe8fe1b8970364f430b
Shibu TN <tnshibu@yahoo.com>
2018-10-11 15:37:33 +0530

    to-do.txt

--- master.sql
+++ master.sql
@@ -2,5 +2,6 @@
 @.\metro-train-service\src\main\resources\train_data.sql
 @.\metro-route-service\src\main\resources\route_schema.sql
 @.\metro-route-service\src\main\resources\route_data.sql
+@.\metro-userregistration-service\src\main\resources\userreg_schema.sql
 
 --master
