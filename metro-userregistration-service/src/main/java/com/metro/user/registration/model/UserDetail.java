796bcff495578da1923da0b37f04f543318ab010
Akhil Narendran <atnarend@I-FLEX.COM>
2018-10-29 13:32:59 +0530

    add checnges to work with login service

--- metro-userregistration-service/src/main/java/com/metro/user/registration/model/UserDetail.java
+++ metro-userregistration-service/src/main/java/com/metro/user/registration/model/UserDetail.java
@@ -1,127 +1,98 @@
 package com.metro.user.registration.model;
 
-import javax.persistence.*;
+import java.util.Set;
 
-@Entity
-@Table(name = "USER_DETAIL")
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+@Entity(name = "UserDetail")
+@Table(name = "USER_DET")
 public class UserDetail {
-
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_SEQ")
-	@SequenceGenerator(sequenceName = "USERDET_SQ", allocationSize = 1, name = "USER_SEQ")
-	Long id;
-
-	@Column(name = "userid")
-	private String userid;
-
-	@Column(name = "password")
-	private int password;
-
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USERDET_SEQ")
+    @SequenceGenerator(sequenceName = "USERDET_SEQ", allocationSize = 1, name = "USERDET_SEQ")
+	@Column(name="USER_ID")
+    Long id;
+	
 	@Column(name = "email")
+	@Email(message = "*Please provide a valid Email")
+	@NotEmpty(message = "*Please provide an email")
 	private String email;
-
-	@Column(name = "username")
-	private String username;
-
-	@Column(name = "gender")
-	private char gender;
-
-	public UserDetail() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public UserDetail(String userid, int password, String email, String username, char gender) {
-		super();
-		this.userid = userid;
-		this.password = password;
-		this.email = email;
-		this.username = username;
-		this.gender = gender;
-	}
-
-	@Override
-	public String toString() {
-		return "UserDetail [id=" + id + ", userid=" + userid + ", password=" + password + ", email=" + email
-				+ ", username=" + username + ", gender=" + gender + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + gender;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + password;
-		result = prime * result + ((userid == null) ? 0 : userid.hashCode());
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		UserDetail other = (UserDetail) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
+	
+	@Column(name = "password")
+	@Length(min = 5, message = "*Your password must have at least 5 characters")
+	@NotEmpty(message = "*Please provide your password")
+	private String password;
+	
+	@Column(name = "name")
+	@NotEmpty(message = "*Please provide your name")
+	private String name;
+	
+	@Column(name = "last_name")
+	@NotEmpty(message = "*Please provide your last name")
+	private String lastName;
+	
+	@Column(name = "active")
+	private int active;
+	
+	@ManyToMany(cascade = CascadeType.MERGE)
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles;
+	
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	public String getUserid() {
-		return userid;
-	}
-
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-
-	public int getPassword() {
-		return password;
-	}
-
-	public void setPassword(int password) {
-		this.password = password;
-	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	public String getUsername() {
-		return username;
+	public String getPassword() {
+		return password;
 	}
-
-	public void setUsername(String username) {
-		this.username = username;
+	public void setPassword(String password) {
+		this.password = password;
 	}
-
-	public char getGender() {
-		return gender;
+	public String getName() {
+		return name;
 	}
-
-	public void setGender(char gender) {
-		this.gender = gender;
+	public void setName(String name) {
+		this.name = name;
 	}
-
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getActive() {
+		return active;
+	}
+	public void setActive(int active) {
+		this.active = active;
+	}
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
 }
